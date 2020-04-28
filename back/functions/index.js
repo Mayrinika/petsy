1258f8b12d2d49e44274b9a9d28d72d903e71414
Mayrinika <36811443+Mayrinika@users.noreply.github.com>
2020-04-28 17:24:11 +0500

    add express & formatting

--- back/functions/index.js
+++ back/functions/index.js
@@ -3,36 +3,35 @@ const admin = require('firebase-admin');
 
 admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-    response.send("Hello from Firebase!");
-});
+const express = require('express');
+const app = express();
 
-exports.getReviews = functions.https.onRequest((req, res) => {
+app.get('/reviews', (req, res) => {
     admin
         .firestore()
         .collection('reviews')
+        .orderBy('createdAt', 'desc')
         .get()
         .then(data => {
             let reviews = [];
             data.forEach(doc => {
-                reviews.push(doc.data());
+                reviews.push({
+                    reviewId: doc.id,
+                    body: doc.data().body,
+                    userHandle: doc.data().userHandle,
+                    createdAt: doc.data().createdAt,
+                });
             });
             return res.json(reviews);
         })
         .catch(err => console.error(err));
 });
 
-exports.createReview = functions.https.onRequest((req, res) => {
-    if (req.method !== 'POST') {
-        return res.status(400).json({error: 'Method non allowed'});
-    }
+app.post('/review', (req, res) => {
     const newReview = {
         body: req.body.body,
         userHandle: req.body.userHandle,
-        createdAt: admin.firestore.Timestamp.fromDate(new Date())
+        createdAt: new Date().toISOString(),
     };
 
     admin
@@ -48,3 +47,5 @@ exports.createReview = functions.https.onRequest((req, res) => {
         })
 
 });
+
+exports.api = functions.https.onRequest(app);
