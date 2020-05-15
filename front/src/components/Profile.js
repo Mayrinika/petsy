de9602792671e872f78c09bbcdcdec543aeca3a2
Mayrinika <36811443+Mayrinika@users.noreply.github.com>
2020-05-15 19:04:03 +0500

    image upload

--- front/src/components/Profile.js
+++ front/src/components/Profile.js
@@ -7,12 +7,13 @@ import routes from './RouterPaths';
 //Styles
 import {withStyles} from "@material-ui/core";
 //MUI stuff
-import {Button, Link as MuiLink, Paper} from '@material-ui/core';
+import {Button, Link as MuiLink, Paper, IconButton, Tooltip} from '@material-ui/core';
 import Typography from '@material-ui/core/Typography';
 //Icons
-import {LocationOn, CalendarToday, Link as LinkIcon} from '@material-ui/icons';
+import {LocationOn, CalendarToday, Link as LinkIcon, Edit as EditIcon} from '@material-ui/icons';
 //Redux
 import {connect} from 'react-redux';
+import {logoutUser, uploadImage} from "../redux/actions/userActions";
 
 const styles = (theme) => ({
     paper: {
@@ -63,6 +64,20 @@ const styles = (theme) => ({
 });
 
 class Profile extends React.Component {
+
+    handleImageChange = (event) => {
+        const image = event.target.files[0];
+        //send to server
+        const formData = new FormData();
+        formData.append('image', image, image.name);
+        this.props.uploadImage(formData); //userActions
+    };
+
+    handleEditPicture = () => {
+        const fileInput = document.getElementById('imageInput');
+        fileInput.click();
+    };
+
     render() {
         const {
             classes,
@@ -78,6 +93,12 @@ class Profile extends React.Component {
                 <div className={classes.profile}>
                     <div className='image-wrapper'>
                         <img src={imageUrl} alt='profile' className='profile-image'/>
+                        <input type='file' id='imageInput' hidden='hidden' onChange={this.handleImageChange}/>
+                        <Tooltip title='Выбрать фото профиля' placement='top'>
+                            <IconButton onClick={this.handleEditPicture} className='button'>
+                                <EditIcon color='primary'/>
+                            </IconButton>
+                        </Tooltip>
                     </div>
                     <hr/>
                     <div className='profile-details'>
@@ -95,7 +116,7 @@ class Profile extends React.Component {
                             </Fragment>
                         )}
                         <CalendarToday color='primary'/> {' '}
-                        <span>С нами с {dayjs(createdAt).format('MMM YYYY')}</span>
+                        <span>Joined {dayjs(createdAt).format('MMM YYYY')}</span>
                     </div>
                 </div>
             </Paper>
@@ -123,7 +144,7 @@ class Profile extends React.Component {
                     </Button>
                 </div>
             </Paper>
-        )) : (<p>loading...</p>);
+        )) : (<p>Загрузка...</p>);
 
         return profileMarkup;
     }
@@ -132,10 +153,17 @@ class Profile extends React.Component {
 Profile.propTypes = {
     classes: PropTypes.object.isRequired,
     user: PropTypes.object.isRequired,
+    logoutUser: PropTypes.func.isRequired,
+    uploadImage: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = (state) => ({
     user: state.user,
 });
 
-export default connect(mapStateToProps)(withStyles(styles)(Profile));
+const mapActionsToProps = {
+    logoutUser,
+    uploadImage,
+};
+
+export default connect(mapStateToProps, mapActionsToProps)(withStyles(styles)(Profile));
